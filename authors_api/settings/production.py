e71b83762dec85bd4098625b963b969c99e5acba
younoch <younoch295@outlook.com>
2024-10-15 13:09:18 +0600

    chore: Setup django project

--- authors_api/settings/production.py
+++
@@ -1,7 +0,0 @@
-from .base import * #noqa
-from .base import env
-
-ADMIN = ['Mohammad Younoch', 'younoch295@gmail.com']
-
-# TODO: add domain names of the production server
-CSRF_TRUSTED_ORIGINS = ['']
