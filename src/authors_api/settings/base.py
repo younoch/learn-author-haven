233f4de2780c2c559b01e15985d01a8dadd16ea4
younoch <younoch295@outlook.com>
2024-10-25 12:40:03 +0600

    user aritcle resonse add, delete

--- src/authors_api/settings/base.py
+++ src/authors_api/settings/base.py
@@ -48,6 +48,7 @@ LOCAL_APPS = [
     "core_apps.articles",
     "core_apps.ratings",
     "core_apps.bookmarks",
+    "core_apps.responses",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
