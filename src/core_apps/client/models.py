0e371137a1df85c43699d6911ab6f4bb2b5f2116
younoch <younoch295@outlook.com>
2024-12-16 19:56:26 +0600

    reset database and organization_ID UUid and UniqueConstraint email and phone in client

--- src/core_apps/client/models.py
+++ src/core_apps/client/models.py
@@ -1,6 +1,4 @@
-from django.db import models
-
-# Create your models here.
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
@@ -8,7 +6,7 @@ from core_apps.common.models import TimeStampedModel
 from core_apps.organization.models import Organization
 
 class Client(TimeStampedModel):
-    organization_id = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="clients")
+    organization_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=255, verbose_name=_("Client Name"))
     address = models.TextField(verbose_name=_("Address"))
     email = models.EmailField(verbose_name=_("Email"))
@@ -16,3 +14,15 @@ class Client(TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["email", "organization_id"],
+                name="unique_email_per_organization"
+            ),
+            models.UniqueConstraint(
+                fields=["phone_number", "organization_id"],
+                name="unique_phone_number_per_organization"
+            ),
+        ]
